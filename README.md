6ff5a3a6cb539a609188930740579a96f3f8def5
Yu Chen <ckymacn@gmail.com>
2016-12-30 18:43:50 +0800

    Added the first submission

---
+++ README.md
@@ -0,0 +1,4 @@
+OCPJavaSE7
+==========
+
+Codes and practice of the OCP Java SE 7 Certificate Guide - Prepare for the 1Z0-804 exam
---
+++ src/edu/cy/chapter4/TestGenerics.java
@@ -0,0 +1,10 @@
+package edu.cy.chapter4;
+
+/**
+ * Created by CY on 12/30/2016.
+ */
+public class TestGenerics {
+    public static void main(String[] args) {
+        System.out.println("TestGenerics");
+    }
+}
